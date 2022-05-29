a0eb653531c39133985ebccd7b1cd076d99bcb42
MAvi-Rup <avirupmondal19@gmail.com>
2022-05-30 02:15:30 +0600

    Problem Solved

--- src/Pages/hooks/useToken.js
+++ src/Pages/hooks/useToken.js
@@ -6,7 +6,7 @@ const useToken = user =>{
         const email = user?.user?.email;
         const currentUser = {email: email};
         if(email){
-            fetch(`https://secret-dusk-46242.herokuapp.com/user/${email}`, {
+            fetch(`https://arcane-earth-34229.herokuapp.com/user/${email}`, {
                 method:'PUT',
                 headers: {
                     'content-type': 'application/json'
